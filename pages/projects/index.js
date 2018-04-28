108e1699318cd548f172a9556d7f88ea6c962351
handaoui-mohamed <handaoui.mohamed@gmail.com>
2018-04-28 17:33:45 +0100

    added the project details dialog

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -4,8 +4,63 @@ import Router from 'next/router'
 import FontAwesome from 'react-fontawesome'
 import Page from '../../components/page'
 
+const ProjectDetails = ({ hide }) => (
+  <div style={{
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'fixed',
+    top: '0',
+    left: '0',
+    height: '100%',
+    width: '100%',
+    backgroundColor: 'rgba(0,0,0,0.5)'
+  }}>
+    <div style={{
+      width: '50%',
+      height: '70%',
+      display: 'flex',
+      flexDirection: 'column',
+      backgroundColor: '#fff'
+    }}>
+      <div style={{
+        width: '100%',
+        height: '400px',
+        background: "url('/static/imgs/main_background.jpg')",
+        backgroundSize: 'cover',
+        position: 'relative'
+      }}>
+        <div onClick={hide} style={{
+          position: 'absolute',
+          top: '10px',
+          right: '10px',
+          backgroundColor: '#fff',
+          width: '20px',
+          borderRadius: '10px',
+          textAlign: 'center',
+          cursor: 'pointer'
+        }}>X</div>
+      </div>
+      <div style={{ flex: 2, padding: '10px' }}>
+        <div>Books2Benefit Charity Book Sale 2017</div>
+        <div>Apr 28 - May 4, 2018</div>
+        <div>NGO: Care2Share</div>
+        <p>
+          Dear Friends and Patrons,
 
-const ProjectPreview = () => (
+          This is to let you know that the Books2Benefit Charity Booksale has been postponed from September 7 and 8, 2017 to November 24 and 25, 2017 to encourage wider participation from across the country. Kindly note we will be continuing to receive book donations and will be activating the Volunteer registrations towards the beginning of October, 2017. Meanwhile, do register you interests on www.books2benefit.me.
+          Thank you so much for all your support!
+
+          Regards,
+          The Books2Benefit UAE Team
+      </p>
+      </div>
+    </div>
+  </div>
+)
+
+
+const ProjectPreview = ({ show }) => (
   <div style={{
     display: 'flex',
     flexDirection: 'row',
@@ -39,7 +94,7 @@ const ProjectPreview = () => (
         Regards,
         The Books2Benefit UAE Team
       </p>
-      <button style={{
+      <button onClick={show} style={{
         backgroundColor: 'transparent',
         outline: 'none',
         color: '#0099cc',
@@ -53,6 +108,21 @@ const ProjectPreview = () => (
 )
 
 export default class extends Page {
+  constructor(props) {
+    super(props)
+
+    this.state = { showDetails: false }
+  }
+
+  showDetails = () => {
+    console.log(this.state)
+    this.setState({ showDetails: true })
+  }
+
+  closeDetails = () => {
+    this.setState({ showDetails: false })
+  }
+
   render() {
     return (
       <div style={{
@@ -88,10 +158,10 @@ export default class extends Page {
           padding: '20px'
         }}>
           {/* project container */}
-          <ProjectPreview />
-          <ProjectPreview />
-          <ProjectPreview />
-          <ProjectPreview />
+          <ProjectPreview show={this.showDetails.bind(this)} />
+          <ProjectPreview show={this.showDetails.bind(this)} />
+          <ProjectPreview show={this.showDetails.bind(this)} />
+          <ProjectPreview show={this.showDetails.bind(this)} />
         </div>
 
         <div style={{ textAlign: 'center' }}>
@@ -102,6 +172,10 @@ export default class extends Page {
             Show more
           </button>
         </div>
+        {
+          this.state.showDetails &&
+          <ProjectDetails hide={() => this.setState({ showDetails: false })} />
+        }
       </div>
     )
   }
