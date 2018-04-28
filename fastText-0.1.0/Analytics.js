67c31c43debb5f7f1904669be6e4d6f56dde2cd9
TofunmiKupoluyi <tofunmi@live.com>
2018-04-28 19:09:48 +0400

    Pushing full fastText with updated Analytics.js

--- fastText-0.1.0/Analytics.js
+++ fastText-0.1.0/Analytics.js
@@ -1,11 +1,8 @@
 exports.Analytics =  class Analytics{
-  constructor(){
-    const {spawn} = require('child_process').spawnSync;
-  }
   static analyzeTweets(tweet, numberOfOutputs){
-    var child = require('child_process').spawnSync('./fasttext', ['predict', 'model_train.bin', '-', numberOfOutputs.toString()], {input: tweet+"\n"});
+    var child = require('child_process').spawnSync('./fasttext', ['predict', 'model_output2.bin', '-', numberOfOutputs.toString()], {input: tweet+"\n", cwd: __dirname});
     return child.stdout.toString().replace("\n","").replace(new RegExp("__label__", 'g'), "").split(" ");
   }
 }
 
-console.log(exports.Analytics.analyzeTweets("Hello", 2))
+
